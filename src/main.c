3bb26dfc85841223a21f6b0369c448d8fe7e786c
vitor-de-oliveira <oliveira.vitormartins@gmail.com>
2023-02-09 14:10:50 -0300

    Small change on basin_entropy_vs_box_size(): divided gibbs_entropy_on_box by the number of attractors.

--- src/main.c
+++ src/main.c
@@ -382,15 +382,15 @@ int main(int argc, char **argv)
 
 	e_step = (e_final - e_initial) / (double)(number_of_e);
 
-	for (int i = 0; i <= number_of_e; i++)
-	{
-		e = e_initial + (double)i * e_step;
-		printf("e = %1.3f\n", e);
+	// for (int i = 0; i <= number_of_e; i++)
+	// {
+	// 	e = e_initial + (double)i * e_step;
+	// 	printf("e = %1.3f\n", e);
 
-		fill_attractor_array(&number_of_pos, &multiple_pos, system, analysis);
+		// fill_attractor_array(&number_of_pos, &multiple_pos, system, analysis);
 
-		if (number_of_pos > 0)
-		{
+		// if (number_of_pos > 0)
+		// {
 			/* calculation on grid */
 	
 			// multiple_basin_of_attraction_determined (number_of_pos, multiple_pos, system, analysis);
@@ -413,18 +413,18 @@ int main(int argc, char **argv)
 			// comparison_entropy_grid_vs_monte_carlo (number_of_pos, multiple_pos, system, analysis);
 			// plot_comparison_entropy_grid_vs_monte_carlo (system, analysis);
 
-			for (int j = 0; j < number_of_pos; j++)
-			{
-				dealloc_2d_double(&multiple_pos[j].orbit, multiple_pos[j].period);
-			}
-			free(multiple_pos);
-		}
-		else
-		{
-			printf("Warning: null number of attractors.\n");
-		}
+		// 	for (int j = 0; j < number_of_pos; j++)
+		// 	{
+		// 		dealloc_2d_double(&multiple_pos[j].orbit, multiple_pos[j].period);
+		// 	}
+		// 	free(multiple_pos);
+		// }
+		// else
+		// {
+		// 	printf("Warning: null number of attractors.\n");
+		// }
 
-	}
+	// }
 
 	// plot_size_multiple_basin_of_attraction_determined_range_e(number_of_e,
 	// 	e_initial, e_final, system, analysis);
